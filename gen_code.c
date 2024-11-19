a85727bdf38f948ada9e7eaa36cf5d18e51e4391
CalebSFeng <caleb.s.feng@gmail.com>
2024-11-19 14:02:37 -0500

    feat: gen_code_output_seq, and gen_code_program_header

---
+++ gen_code.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+#include "spl.tab.h"
+#include "utilities.h"
+#include "ast.h"
+#include "bof.h"
+#include "instruction.h"
+#include "code.h"
+#include "regname.h"
+#include "id_use.h"
+#include "literal_table.h"
+#include "gen_code.h"
+#include "code_seq.h"
+
+void gen_code_initialize() 
+{
+    literal_table_initialize();
+}
+
+// Adapted from FLOAT
+// Requires: bf if open for writing in binary
+// and prior to this scope checking and type checking have been done.
+// Write all the instructions in cs to bf in order
+static void gen_code_output_seq(BOFFILE bf, code_seq cs)
+{
+    while (!code_seq_is_empty(cs)) {
+	bin_instr_t inst = code_seq_first(cs)->instr;
+	instruction_write_bin_instr(bf, inst);
+	cs = code_seq_rest(cs);
+    }
+}
+
+// Adapted from FLOAT
+// Return a header appropriate for the give code
+static BOFHeader gen_code_program_header(code_seq main_cs)
+{
+    BOFHeader ret;
+    strncpy(ret.magic, "MAGIC", 5);  // for FLOAT SRM
+    ret.text_start_address = 0;
+    // remember, the unit of length in the BOF format is a byte!
+    ret.text_length = code_seq_size(main_cs) * BYTES_PER_WORD;
+    int dsa = MAX(ret.text_length, 1024) + BYTES_PER_WORD;
+    ret.data_start_address = dsa;
+    ret.data_length = literal_table_size() * BYTES_PER_WORD;
+    int sba = dsa
+	+ ret.data_start_address
+	+ ret.data_length + ret.text_length + STACK_SPACE;
+    ret.stack_bottom_addr = sba;
+    return ret;
+}
