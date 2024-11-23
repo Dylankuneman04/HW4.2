7fdc6416eac05b52dcb286172ffbc33b4390b1d3
Dylankuneman04 <119005722+Dylankuneman04@users.noreply.github.com>
2024-11-23 11:03:48 -0500

    print, read, stack operations

--- gen_code.h
+++ gen_code.h
@@ -8,4 +8,10 @@
 
 #define STACK_SPACE 4096
 
+void gen_print_str(code_seq *seq, const char *str);
+void gen_print_int(code_seq *seq, int num);
+void gen_read_char(code_seq *seq);
+void gen_print_char(code_seq *seq, char c);
+void gen_push_number(code_seq *seq, int number);
+
 #endif
